a5c447d15cb58430c4931fd4b95ca14c0bd345b4
Paul Fournier <pfournier597@gmail.com>
2022-10-25 16:32:12 +0200

    some add on the race controller

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,2 +1,13 @@
 class RacesController < ApplicationController
+  def show
+    @race = Race.find(params[:id])
+  end
+
+  def new
+    @race = Race.new
+  end
+
+  def edit
+    @race = Race.find(params[:id])
+  end
 end
